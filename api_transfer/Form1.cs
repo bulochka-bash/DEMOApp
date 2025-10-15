7ce178a380bf77d8cd358c016145ac009df12395
bulochka-bash <aydarbulat41@gmail.com>
2025-10-15 12:56:13 +0300

    api

---
+++ api_transfer/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace api_transfer
+{
+    public partial class Form1 : Form
+    {
+        HttpClient client = new HttpClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        async private Task<string> GetName(string path)
+        {
+            string resp = "";
+            HttpResponseMessage responce = await client.GetAsync(path);
+            if (responce.IsSuccessStatusCode)
+            {
+                resp = await responce.Content.ReadAsStringAsync();
+            }
+            return resp;
+        }
+
+        async private void button1_Click(object sender, EventArgs e)
+        {
+            string path = "http://localhost:4444/TransferSimulator/fullName";
+            string outputJson = await GetName(path);
+            UserName name =  JsonSerializer.Deserialize<UserName>(outputJson);
+            if (IsValidName(name.value))
+            {
+                MessageBox.Show("ФИО верно");
+            }
+            else
+            {
+                MessageBox.Show("ФИО  noверно");
+            }
+            label1.Text = name.value;
+        }
+        private bool IsValidName(string name) 
+        {
+            char[] nameByChars = name.ToCharArray();
+            foreach (char c in nameByChars)
+            {
+                
+                if (!Char.IsLetter(c))
+                {
+                    if (Char.IsWhiteSpace(c))
+                    {
+
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    
+                }
+            }
+            return true;
+        }
+    }
+    public class UserName
+    {
+        public string value { get; set; }
+    }
+}
---
+++ api_transfer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("api_transfer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("sborka")]
+[assembly: AssemblyProduct("api_transfer")]
+[assembly: AssemblyCopyright("Copyright © sborka 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("10a8e865-ba4d-4b76-9893-501f452df095")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
