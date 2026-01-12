bc34f3d437e2552b0a72043a0f2cc83f7ab59875
bulochka-bash <aydarbulat41@gmail.com>
2026-01-12 18:17:12 +0300

    chut neto no toje norm

--- api_transfer/Form1.cs
+++ api_transfer/Form1.cs
@@ -41,12 +41,12 @@ namespace api_transfer
 
             this.name.Text = name.value;
         }
-        private bool IsValidName(string name) 
+        private bool IsValidName(string name)
         {
             char[] nameByChars = name.ToCharArray();
             foreach (char c in nameByChars)
             {
-                
+
                 if (!Char.IsLetter(c))
                 {
                     if (Char.IsWhiteSpace(c))
@@ -57,7 +57,7 @@ namespace api_transfer
                     {
                         return false;
                     }
-                    
+
                 }
             }
             return true;
@@ -67,13 +67,14 @@ namespace api_transfer
         {
             if (IsValidName(name.Text))
             {
-                MessageBox.Show("ФИО верно","Успех",MessageBoxButtons.OK,MessageBoxIcon.Information);
-                SaveFile();
+                MessageBox.Show("ФИО верно", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                SaveWordFile("ФИО не содержит запрещенные символы");
             }
             else
             {
                 MessageBox.Show("ФИО неверно", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 wrongNameLabel.Text = "ФИО содержит запрещенные символы";
+                SaveWordFile("ФИО содержит запрещенные символы");
             }
         }
         private void SaveFile()
@@ -81,29 +82,54 @@ namespace api_transfer
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Title = "Export To Word";
             saveFileDialog.Filter = "To Word (Word)|*.docx";
-            saveFileDialog.FileName = "tables";
-            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            saveFileDialog.FileName = "word";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
-                CreateWordFile(saveFileDialog.FileName);
+                //SaveWordFile(saveFileDialog.FileName);
             }
         }
-        private void CreateWordFile(string fileName)
+        private void SaveWordFile(string result)
         {
             Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
-            Microsoft.Office.Interop.Word.Document wordDocument = wordApp.Documents.Add();
+            Document document = wordApp.Documents.Open(OpenWord());
 
-            Microsoft.Office.Interop.Word.Paragraph paragraph =  wordDocument.Paragraphs.Add();
-            Microsoft.Office.Interop.Word.Range range = paragraph.Range;
-
-            range.Text = name.Text;
-            wordDocument.SaveAs2(fileName);
-            wordDocument.Close();
+            int start = document.Bookmarks["start"].Start;
+            int end = document.Bookmarks["end"].End;
+            document.Bookmarks["end"].Delete();
+            Range range = document.Range(start,end);
+            range.Select();
+            range.Font.Size = 14f;
+            range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+            range.Text = result;
+            document.Bookmarks.Add("end", document.Range(range.End-1,range.End));
+            document.Save();
+            document.Close();
             wordApp.Quit();
         }
-        
+        private string OpenWord()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Word files (*.docx)|*.docx";
+            openFileDialog.Title = "Open word file";
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                return openFileDialog.FileName;
+            }
+            return null;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+        }
     }
+}
+        
+       
+        
+    
     public class UserName
     {
         public string value { get; set; }
     }
-}
+
